5a90b2688d9ef5251fe95841adfd3110091c7af6
Carles <cgt.21@hotmail.com>
2020-01-15 11:59:30 +0100

    Exercici 1 de variables

---
+++ Exercici_Fase1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Exercici_Fase1
+    {
+        static void Main(string[] args)
+        {
+            string nom = "Carles";
+            string cognom1 = "Gascó";
+            string cognom2 = "Tomàs";
+
+            int dia = 13;
+            int mes = 1;
+            int any = 2020;
+
+            Console.WriteLine($"{cognom1} {cognom2}, {nom}");
+            Console.WriteLine($"{dia}/{mes}/{any}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
